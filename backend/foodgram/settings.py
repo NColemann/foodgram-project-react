523e8aa9bb1b7fc56afc5da0c0901b4b6ec99f14
NColemann <n.kalimanov@yandex.ru>
2022-08-24 19:06:27 +0300

    add git actions

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'po1pg)(70-kvf!nwrqn)ty)@1qw!wi9(@k*z()o3&_a24@&7uo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
