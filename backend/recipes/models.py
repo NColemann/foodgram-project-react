e405973046614e794e112c52dea1e69621d50d81
NColemann <n.kalimanov@yandex.ru>
2022-08-26 19:00:18 +0300

    fix

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -83,7 +83,7 @@ class Recipe(models.Model):
     class Meta:
         ordering = ('-pub_date',)
         verbose_name = 'Рецепт'
-        verbose_name_plural = 'Рецептs'
+        verbose_name_plural = 'Рецепты'
 
     def __str__(self):
         return self.text
